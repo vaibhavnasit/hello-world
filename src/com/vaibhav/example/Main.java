e237c2744170c7d1665fa0b44e37aa90ec628c11
vaibhavnasit <vaibhavnasit@yahoo.com>
2019-03-28 04:48:39 +0530

    initial commit for hello-world java project

---
+++ src/com/vaibhav/example/Main.java
@@ -0,0 +1,9 @@
+package com.vaibhav.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My World!! My way...");
+    }
+}
